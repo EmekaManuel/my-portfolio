f764f24271d8c0943a7838c44f665a01b34cd0c5
EmekaManuel <marrnuel12@gmail.com>
2023-02-16 09:38:51 +0100

    footer changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <div  name='footer' className=' bg-gradient-to-b from-black to-gray-800 p-4 text-white'>
         <div className='flex justify-center max-w-screen-lg mx-auto  h-full'>
             <div className=''>
-                <p className='text-[20px]'>&#169; Onyewuchi Emeka Manuel. 2022</p>
+                <p className='font-medium text-gray-300'>&#169; Onyewuchi Emeka Manuel. 2022</p>
             </div>
         </div>
     </div>
